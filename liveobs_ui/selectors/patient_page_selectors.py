06103239b2b8578bafc6e695373e93141b550a36
Colin Wren <colin.wren@bjss.com>
2017-06-07 11:22:54 +0100

    - Ad Hoc Observation menu uses an ID not a Class!

--- liveobs_ui/selectors/patient_page_selectors.py
+++ liveobs_ui/selectors/patient_page_selectors.py
@@ -4,8 +4,8 @@ from selenium.webdriver.common.by import By
 
 ADHOC_OBS_MENU_BUTTON = (By.ID, 'take-observation')
 OPEN_OBS_MENU = (By.ID, 'obs_menu')
-OPEN_OBS_MENU_TITLE = (By.CSS_SELECTOR, '.obs_menu h2')
-OPEN_OBS_MENU_LIST_ITEMS = (By.CSS_SELECTOR, '.obs_menu .menu li a')
-OPEN_OBS_MENU_NEWS = (By.CSS_SELECTOR, '#obsMenu li a:first-child')
+OPEN_OBS_MENU_TITLE = (By.CSS_SELECTOR, '#obs_menu h2')
+OPEN_OBS_MENU_LIST_ITEMS = (By.CSS_SELECTOR, '#obs_menu .menu li a')
+OPEN_OBS_MENU_NEWS = (By.CSS_SELECTOR, '#obs_menu li a:first-child')
 OPEN_OBS_MENU_CLOSE_BUTTON = \
-    (By.CSS_SELECTOR, '.obs_menu options li:first-child() a')
+    (By.CSS_SELECTOR, '#obs_menu options li:first-child() a')
