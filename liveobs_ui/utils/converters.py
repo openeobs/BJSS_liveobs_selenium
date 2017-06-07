44e0a07cf6b7364927a70c358d2dde00f4e7a01d
Colin Wren <colin.wren@bjss.com>
2017-06-07 12:13:53 +0100

    - Adding func to convert a list of data dicts

--- liveobs_ui/utils/converters.py
+++ liveobs_ui/utils/converters.py
@@ -13,3 +13,16 @@ def convert_numbers_in_dict(values_dict):
         if isinstance(value, (int, float)):
             new_dict[key] = str(value)
     return new_dict
+
+
+def convert_list_of_data_dicts(values_list):
+    """
+    Convert a list of dicts that contain data to fill out a form
+
+    :param values_list: List of dictionaries that contain form data
+    :return: list of dictionaries with converted values
+    """
+    new_list = values_list.copy()
+    for item in new_list:
+        item = convert_numbers_in_dict(item)
+    return new_list
